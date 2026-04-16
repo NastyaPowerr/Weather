dea800d70c1d4f641a77704aa905a16bd3f9613d
NastyaPowerr <cake.delicious@mail.ru>
2026-04-16 17:05:36 +0300

    feat(test): add auth tests

---
+++ src/test/java/org/roadmap/weather/config/TestConfig.java
@@ -0,0 +1,32 @@
+package org.roadmap.weather.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("org.roadmap.weather.service")
+@ComponentScan("org.roadmap.weather.repository")
+@PropertySource("classpath:application-test.properties")
+public class TestConfig {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5433/postgres?currentSchema=weather_test");
+        dataSource.setUsername("postgres_test");
+        dataSource.setPassword("12345");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
