9ddb6df6d7218fccc023abecd8d23bcc99e59fd6
NastyaPowerr <cake.delicious@mail.ru>
2026-05-07 13:59:23 +0300

    feat: add updated cache for external API locations

--- src/main/java/org/roadmap/weather/service/LocationService.java
+++ src/main/java/org/roadmap/weather/service/LocationService.java
@@ -7,8 +7,8 @@ import org.roadmap.weather.dto.UserDto;
 import org.roadmap.weather.dto.response.LocationResponseDto;
 import org.roadmap.weather.entity.Location;
 import org.roadmap.weather.exception.ExceptionMessages;
-import org.roadmap.weather.exception.ValidationException;
 import org.roadmap.weather.exception.ExternalApiParseException;
+import org.roadmap.weather.exception.ValidationException;
 import org.roadmap.weather.exception.location.GeocodingApiCallException;
 import org.roadmap.weather.mapper.LocationMapper;
 import org.roadmap.weather.repository.LocationRepository;
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestClientException;
@@ -58,6 +59,29 @@ public class LocationService {
                 ));
     }
 
+    @Cacheable(cacheNames = "locations", key = "#userId")
+    public List<Location> findByUserId(Integer userId) {
+        return locationRepository.findByUserId(userId);
+    }
+
+    @Transactional
+    @CacheEvict(cacheNames = "locations", key = "#userId")
+    public void delete(String locationId, Integer userId) {
+        try {
+            Integer id = Integer.valueOf(locationId);
+            List<Location> locations = locationRepository.findByUserId(userId);
+            for (Location location : locations) {
+                if (location.getId().equals(id)) {
+                    locationRepository.deleteById(id);
+                    log.info("User={} deleted location={}", userId, locationId);
+                }
+            }
+        } catch (NumberFormatException ex) {
+            throw new ValidationException(ExceptionMessages.LOCATION_NOT_FOUND_FOR_USER);
+        }
+    }
+
+    @Cacheable(cacheNames = "openWeatherMapLocations", key = "#locationName")
     public List<LocationDto> findByName(String locationName) {
         String url = String.format(
                 "https://api.openweathermap.org/geo/1.0/direct?q=%s&limit=%s&appid=%s&lang=%s",
@@ -95,25 +119,9 @@ public class LocationService {
         }
     }
 
-    @Transactional
-    @CacheEvict(cacheNames = "locations", key = "#userId")
-    public void delete(String locationId, Integer userId) {
-        try {
-            Integer id = Integer.valueOf(locationId);
-            List<Location> locations = locationRepository.findByUserId(userId);
-            for (Location location : locations) {
-                if (location.getId().equals(id)) {
-                    locationRepository.deleteById(id);
-                    log.info("User={} deleted location={}", userId, locationId);
-                }
-            }
-        } catch (NumberFormatException ex) {
-            throw new ValidationException(ExceptionMessages.LOCATION_NOT_FOUND_FOR_USER);
-        }
-    }
-
-    @Cacheable(cacheNames = "locations", key = "#userId")
-    public List<Location> findByUserId(Integer userId) {
-        return locationRepository.findByUserId(userId);
+    @Scheduled(cron = "0 0 10 * * *")
+    @CacheEvict(cacheNames = "openWeatherMapLocations", allEntries = true)
+    public void evictLocationCache() {
+        log.info("Evicted openWeatherApi locations cache.");
     }
 }
