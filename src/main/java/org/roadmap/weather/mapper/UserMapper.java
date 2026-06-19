4eaa241fa0e26f2d747f19a788832d333821ad12
NastyaPowerr <cake.delicious@mail.ru>
2026-06-19 17:58:57 +0300

    fix: fix username/login info for mapper

--- src/main/java/org/roadmap/weather/mapper/UserMapper.java
+++ src/main/java/org/roadmap/weather/mapper/UserMapper.java
@@ -1,10 +1,12 @@
 package org.roadmap.weather.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.roadmap.weather.dto.UserDto;
 import org.roadmap.weather.entity.User;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(source = "login", target = "username")
     UserDto toDto(User user);
 }
